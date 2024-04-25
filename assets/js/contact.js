ce707db726cf27d57cb706fbb6372e57abe7d214
DevDanielaCN <dani.cn@outlook.es>
2024-04-25 10:33:03 -0500

    header, cv icon and loader formanimation

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,11 @@
 'use strict';
 // VARIABLES GENERALES
 
-let form = document.getElementById('form');
-let btnForm = document.getElementById('btn_form');
-let errorsContainer = document.getElementById('errors');
-let arrayInputs = document.querySelectorAll('[validate-data="true"]');
+const form = document.getElementById('form');
+const btnForm = document.getElementById('btn_form');
+const errorsContainer = document.getElementById('errors');
+const arrayInputs = document.querySelectorAll('[validate-data="true"]');
+const carga = document.getElementById('loader');
 let arrayErrors = [];
 let regexEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
 let maxlengthTxt = 50;
@@ -17,8 +18,15 @@ document.addEventListener("DOMContentLoaded", function(){
 function runEvents() {
     btnForm.disabled = true;
     btnForm.addEventListener("click", function(e) {
+        e.preventDefault();
         validarFormulario();
-        form.onsubmit();
+        // Mostrar la animación de carga
+        carga.style.display = "flex";
+        btnForm.style.display = 'none';
+        setTimeout(function () {
+            form.submit(); // Envía el formulario después de un cierto tiempo (en este caso, después de 5 segundos)
+
+        }, 3000);
     });
    
     arrayInputs.forEach(input => {
@@ -27,7 +35,6 @@ function runEvents() {
 }
 
 function validarFormulario(event) {
-    event.preventDefault();
 
     arrayErrors = [];
 
@@ -40,6 +47,7 @@ function validarFormulario(event) {
         else if( item.getAttribute('id') === 'matter' && item.value !== '' && item.value.length > maxlengthTxt){
             arrayErrors.push(`${item.getAttribute('placeholder')} tiene más de ${maxlengthTxt} caracteres y`);
             item.classList.add('invalid');
+            return
         }
         else if(item.type === 'email' && !regexEmail.test(item.value) && item.value !== ''){
             arrayErrors.push(`${item.getAttribute('placeholder')} no es correcto y`);
@@ -60,6 +68,7 @@ function validarFormulario(event) {
 
     if(!arrayErrors.length){
         btnForm.disabled = false;
+
         errorsContainer.innerHTML = '';
             arrayInputs.forEach(function(item){
                 item.classList.remove('invalid');
